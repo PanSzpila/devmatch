4a581472ce4ce0813bd59df3856c85ad8892c21b
Piotr Podgorski <p.t.podgorski+git@gmail.com>
2026-04-25 17:36:00 +0200

    profiles controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ProfilesController } from './profiles.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, ProfilesController],
   providers: [AppService],
 })
 export class AppModule {}
